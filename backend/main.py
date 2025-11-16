87c25c5e0899d6c59105cd08700a8167969255a1
juansantaella <159500050+juansantaella@users.noreply.github.com>
2025-11-16 13:30:33 -0500

    Correct Duplicated CORS
    Correct Duplicated CORS

--- backend/main.py
+++ backend/main.py
@@ -64,18 +64,6 @@ app = FastAPI(
     version="1.0.0",
 )
 
-# Allow local dev frontends (Vite, etc.)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 # ---------------------------------------------------------------------------
 # Helpers
